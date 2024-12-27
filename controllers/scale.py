4549e6910d7b12a164ab5b21691ae3502ccad303
postos-graciosa-ti <postosgraciosati@gmail.com>
2024-12-27 08:39:35 -0300

    refactor: modularize scales routes

--- controllers/scale.py
+++ controllers/scale.py
@@ -1,5 +1,5 @@
 import json
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from fastapi import HTTPException
 from sqlmodel import Session, select
@@ -9,131 +9,145 @@ from models.function import Function
 from models.scale import Scale
 from models.turn import Turn
 from models.workers import Workers
-from pyhints.scales import GetScalesByDate
+from pyhints.scales import GetScalesByDate, PostScaleInput
 
 
-def handle_get_scale_by_subsidiarie_id(subsidiarie_id: int):
+def handle_get_scales_by_subsidiarie_id(subsidiarie_id: int):
     with Session(engine) as session:
         statement = select(Scale).where(Scale.subsidiarie_id == subsidiarie_id)
 
-        scales = session.exec(statement).all()
+        scales_by_subsidiarie = session.exec(statement).all()
 
-        if not scales:
+        format_scales = []
+
+        for scale in scales_by_subsidiarie:
+            format_scale = {
+                "id": scale.id,
+                # 'worker_id': scale.worker_id,
+                "worker": session.get(Workers, scale.worker_id),
+                "days_on": eval(scale.days_on),
+                "days_off": eval(scale.days_off),
+                "need_alert": scale.need_alert,
+                "proportion": scale.proportion,
+            }
+
+            format_scales.append(format_scale)
+
+    return format_scales
+
+
+def handle_get_scales_by_subsidiarie_and_worker_id(subsidiarie_id: int, worker_id: int):
+    with Session(engine) as session:
+        statement = (
+            select(Scale)
+            .where(Scale.subsidiarie_id == subsidiarie_id)
+            .where(Scale.worker_id == worker_id)
+        )
+
+        scales_by_subsidiarie_and_worker_id = session.exec(statement).first()
+
+    return eval(scales_by_subsidiarie_and_worker_id.days_off)
+
+
+def handle_post_scale(form_data: PostScaleInput):
+    try:
+        form_data.days_off = eval(form_data.days_off)
+
+        first_day = datetime.strptime(form_data.first_day, "%d-%m-%Y")
+
+        last_day = datetime.strptime(form_data.last_day, "%d-%m-%Y")
+
+        dias_do_mes = []
+
+        data_atual = first_day
+
+        while data_atual <= last_day:
+            dias_do_mes.append(data_atual.strftime("%d-%m-%Y"))
+            data_atual += timedelta(days=1)
+
+        # Dias sem folga
+        dias_sem_folga = [dia for dia in dias_do_mes if dia not in form_data.days_off]
+
+        # Calcula proporção e verifica se há mais de 8 dias consecutivos sem folga
+        all_dates = sorted(dias_sem_folga + form_data.days_off)
+        options = [
+            {"dayOff": date in form_data.days_off, "value": date} for date in all_dates
+        ]
+
+        dias_consecutivos = []
+        contador = 0
+        tem_mais_de_oito_dias_consecutivos = False
+
+        for dia in options:
+            if dia["dayOff"]:
+                dias_consecutivos.append({"dias": contador, "dataFolga": dia["value"]})
+                contador = 0
+            else:
+                contador += 1
+                if contador > 8:
+                    tem_mais_de_oito_dias_consecutivos = True
+
+        proporcoes = [
+            {
+                "folga": idx + 1,
+                "data": item["dataFolga"],
+                "proporcao": f"{item['dias']}x1",
+            }
+            for idx, item in enumerate(dias_consecutivos)
+        ]
+
+        # Verificação de erro se não houver dias de folga
+        if not form_data.days_off:
             raise HTTPException(
-                status_code=404, detail="No scales found for the given subsidiary."
+                status_code=400, detail="Não é possível salvar sem dias de folga."
             )
 
-        result = []
+        # Gerenciamento da sessão com contexto `with`
+        with Session(engine) as session:
+            # Verifica se já existe um registro com o mesmo worker_id e subsidiarie_id
+            existing_scale = session.exec(
+                select(Scale).where(
+                    Scale.worker_id == form_data.worker_id,
+                    Scale.subsidiarie_id == form_data.subsidiarie_id,
+                )
+            ).first()
 
-        for scale in scales:
-            workers_on = json.loads(scale.workers_on)
+            if existing_scale:
+                # Atualiza os dados do registro existente
+                existing_scale.days_on = json.dumps(dias_sem_folga)
+                existing_scale.days_off = json.dumps(form_data.days_off)
+                existing_scale.need_alert = tem_mais_de_oito_dias_consecutivos
+                existing_scale.proportion = json.dumps(proporcoes)
+            else:
+                # Cria um novo registro
+                existing_scale = Scale(
+                    worker_id=form_data.worker_id,
+                    subsidiarie_id=form_data.subsidiarie_id,
+                    days_on=json.dumps(dias_sem_folga),
+                    days_off=json.dumps(form_data.days_off),
+                    need_alert=tem_mais_de_oito_dias_consecutivos,
+                    proportion=json.dumps(proporcoes),
+                )
+                session.add(existing_scale)
 
-            workers_off = json.loads(scale.workers_off)
+            # Salva as mudanças no banco
+            session.commit()
+            session.refresh(existing_scale)
 
-            workers_on_data = [
-                session.get(Workers, worker_id) for worker_id in workers_on
-            ]
-
-            workers_off_data = [
-                session.get(Workers, worker_id) for worker_id in workers_off
-            ]
-
-            def format_worker(worker):
-                if worker:
-                    function = session.get(Function, worker.function_id)
-
-                    turn = session.get(Turn, worker.turn_id)
-
-                    return {
-                        "id": worker.id,
-                        "name": worker.name,
-                        "function": {
-                            "id": function.id if function else None,
-                            "name": function.name if function else "Unknown",
-                        },
-                        "turn": {
-                            "id": turn.id if turn else None,
-                            "name": turn.name if turn else "Unknown",
-                        },
-                    }
-                return None
-
-            result.append(
-                {
-                    "scale_id": scale.id,
-                    "date": scale.date,
-                    "workers_on": [
-                        format_worker(worker) for worker in workers_on_data if worker
-                    ],
-                    "workers_off": [
-                        format_worker(worker) for worker in workers_off_data if worker
-                    ],
-                }
-            )
-
-    return result
+        # Retorna os dados atualizados ou inseridos
+        return eval(existing_scale.days_off)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 
-def handle_get_scale_by_date(formData: GetScalesByDate):
-    initial = datetime.strptime(formData.initial_date, "%d/%m/%Y").date()
-
-    end = datetime.strptime(formData.end_date, "%d/%m/%Y").date()
-
+def handle_delete_scale(scale_id: int, subsidiarie_id: int):
     with Session(engine) as session:
-        statement = select(Scale).where(Scale.date >= initial, Scale.date <= end)
-        scales = session.exec(statement).all()
-
-        result = []
-
-        for scale in scales:
-            workers_on = eval(scale.workers_on)
-            workers_off = eval(scale.workers_off)
-
-            workers_on_data = [
-                session.get(Workers, worker_on) for worker_on in workers_on
-            ]
-
-            workers_off_data = [
-                session.get(Workers, worker_off) for worker_off in workers_off
-            ]
-
-            result.append(
-                {
-                    "scale_id": scale.id,
-                    "date": scale.date,
-                    "workers_on": [
-                        {"id": worker.id, "name": worker.name}
-                        for worker in workers_on_data
-                        if worker
-                    ],
-                    "workers_off": [
-                        {"id": worker.id, "name": worker.name}
-                        for worker in workers_off_data
-                        if worker
-                    ],
-                }
-            )
-
-        return result
-
-
-def handle_post_scale(formData: Scale):
-    formData.date = datetime.strptime(formData.date, "%d/%m/%Y").date()
-
-    with Session(engine) as session:
-        session.add(formData)
+        session.delete(session.get(Scale, scale_id))
 
         session.commit()
 
-        session.refresh(formData)
-    return formData
+        statement = select(Scale).where(Scale.subsidiarie_id == subsidiarie_id)
 
+        all_scales_by_subsidiarie = session.exec(statement).all()
 
-def handle_delete_scale(id: int):
-    with Session(engine) as session:
-        scale = session.get(Scale, id)
-
-        session.delete(scale)
-
-        session.commit()
-    return {"message": "Escala deletada com sucesso"}
+    return all_scales_by_subsidiarie
--- pyhints/scales.py
+++ pyhints/scales.py
@@ -1,9 +1,19 @@
 from pydantic import BaseModel
 
+
 class WeekScale(BaseModel):
-  initialDate: str
-  finalDate: str
+    initialDate: str
+    finalDate: str
+
 
 class GetScalesByDate(BaseModel):
     initial_date: str
     end_date: str
+
+
+class PostScaleInput(BaseModel):
+    worker_id: int
+    subsidiarie_id: int
+    days_off: str
+    first_day: str
+    last_day: str

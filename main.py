92f8762c902516a08f8578512d6744e90b3e1f42
postos-graciosa-ti <postosgraciosati@gmail.com>
2025-01-24 10:31:36 -0300

    teste

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 from fastapi import Depends, FastAPI, File, Form, HTTPException, UploadFile
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
-from sqlmodel import Field, Session, SQLModel, create_engine, select
+from sqlmodel import Field, Session, SQLModel, create_engine, or_, select
 
 from controllers.candidates import (
     handle_get_candidates,
@@ -130,7 +130,6 @@ from pyhints.users import (
 )
 from scripts.excel_scraping import handle_excel_scraping
 from seeds.seed_all import seed_database
-from sqlmodel import or_
 
 
 class Test(SQLModel, Table=True):
@@ -880,13 +879,12 @@ def get_scales_report(id: int, scale_report_input: ScalesReportInput):
                 return turn_report
 
             report = {
-                "segundo_turno_report": generate_turn_report(1),  # 14:00 - 22:00
-                "primeiro_turno_report": generate_turn_report(2),  # 06:00 - 14:00
-                "terceiro_turno_report": generate_turn_report(3),  # 22:00 - 06:00
-                "quarto_turno_report": generate_turn_report(4),  # 08:00 - 18:00
-                "quinto_turno_report": generate_turn_report(5),  # 14:00 - 22:00
-                "sexto_turno_report": generate_turn_report(6),  # 18:00 - 02:00
-                "setimo_turno_report": generate_turn_report(7),  # 06:00 - 15:00
+                "primeiro_turno_report": generate_turn_report(2),
+                "segundo_turno_report": generate_turn_report(1),
+                "terceiro_turno_report": generate_turn_report(3),
+                "quarto_turno_report": generate_turn_report(4),
+                "quinto_turno_report": generate_turn_report(5),
+                "sexto_turno_report": generate_turn_report(6),
             }
 
             return report

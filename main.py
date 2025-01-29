9e0d313e457dc4e30ac553fca95048307933d510
postos-graciosa-ti <postosgraciosati@gmail.com>
2025-01-29 11:02:08 -0300

    feature: subsidiaries notifications

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import calendar
 import json
 from datetime import datetime, timedelta
 
@@ -8,7 +9,7 @@ from dotenv import load_dotenv
 from fastapi import Depends, FastAPI, File, Form, HTTPException, UploadFile
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
-from sqlmodel import Field, Session, SQLModel, create_engine, or_, select
+from sqlmodel import Field, Session, SQLModel, create_engine, func, or_, select
 
 from controllers.candidates import (
     handle_get_candidates,
@@ -613,3 +614,87 @@ async def put_department(id: int, department_input: Department):
 @app.delete("/departments/{id}")
 async def delete_department(id: int):
     return await handle_delete_department(id)
+
+
+# notifications
+
+
+def get_workers_without_scales(subsidiarie_id):
+    with Session(engine) as session:
+        all_workers = session.exec(
+            select(Workers).where(Workers.subsidiarie_id == subsidiarie_id)
+        ).all()
+
+        all_scales = session.exec(
+            select(Scale).where(Scale.subsidiarie_id == subsidiarie_id)
+        ).all()
+
+        workers_with_scales = {scale.worker_id for scale in all_scales}
+
+        workers_without_scale = [
+            worker for worker in all_workers if worker.id not in workers_with_scales
+        ]
+
+        return workers_without_scale
+
+
+def get_workers_with_less_than_ideal_days_off(subsidiarie_id):
+    with Session(engine) as session:
+        today = datetime.now()
+
+        year = today.year
+
+        month = today.month
+
+        first_day = datetime(year, month, 1)
+
+        last_day = (first_day + timedelta(days=31)).replace(day=1) - timedelta(days=1)
+
+        sundays = 0
+
+        current_day = first_day
+
+        while current_day <= last_day:
+            if current_day.weekday() == 6:
+                sundays += 1
+            current_day += timedelta(days=1)
+
+        ideal_days_off = sundays + 4
+
+        workers = session.exec(
+            select(Workers)
+            .join(Scale, Workers.id == Scale.worker_id)
+            .where(Scale.subsidiarie_id == subsidiarie_id)
+            .where(func.json_array_length(Scale.days_off) < ideal_days_off)
+        ).all()
+
+        return {"workers": workers, "ideal_days_off": ideal_days_off}
+
+
+def get_open_jobs(subsidiarie_id):
+    with Session(engine) as session:
+        jobs = session.exec(
+            select(Jobs).where(Jobs.subsidiarie_id == subsidiarie_id)
+        ).all()
+
+        return jobs
+
+
+@app.get("/subsidiaries/{id}/notifications")
+async def get_subsidiary_notifications(id: int):
+    workers_without_scales = get_workers_without_scales(id)
+
+    open_jobs = get_open_jobs(id)
+
+    workers_with_less_than_ideal_days_off = get_workers_with_less_than_ideal_days_off(
+        id
+    )
+
+    return {
+        "workers_without_scales": workers_without_scales,
+        "open_jobs": open_jobs,
+        "workers_with_less_than_ideal_days_off": workers_with_less_than_ideal_days_off[
+            "workers"
+        ],
+        "ideal_days_off": workers_with_less_than_ideal_days_off["ideal_days_off"],
+    }

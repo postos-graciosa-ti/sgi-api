0dbf851835bc13c118473149734784b63f545ecd
postos-graciosa-ti <postosgraciosati@gmail.com>
2025-01-31 13:42:01 -0300

    test

--- database/sqlite.py
+++ database/sqlite.py
@@ -1,14 +1,14 @@
-from sqlmodel import Field, Session, SQLModel, create_engine, select
-from dotenv import load_dotenv
 import os
+
+from dotenv import load_dotenv
+from sqlmodel import Field, Session, SQLModel, create_engine, select
 
 load_dotenv()
 
 sqlite_url = os.environ.get("SQLITE_URL")
 
-connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, echo=True)
 
-engine = create_engine(sqlite_url, echo=True, connect_args=connect_args)
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)

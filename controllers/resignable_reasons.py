cf065c1cdd6dc883c08b86398126c89aa7e3c840
postos-graciosa-ti <postosgraciosati@gmail.com>
2025-02-04 11:35:15 -0300

    improvement: crud workers

--- controllers/resignable_reasons.py
+++ controllers/resignable_reasons.py
@@ -1,7 +1,12 @@
-from sqlmodel import Session, select
+import json
+from datetime import datetime
+
+from sqlmodel import Session, and_, select
 
 from database.sqlite import engine
 from models.resignable_reasons import ResignableReasons
+from models.workers import Workers
+from pyhints.resignable_reasons import StatusResignableReasonsInput
 
 
 async def handle_get_resignable_reasons():
@@ -9,3 +14,55 @@ async def handle_get_resignable_reasons():
         resignable_reasons = session.exec(select(ResignableReasons)).all()
 
     return resignable_reasons
+
+
+async def handle_resignable_reasons_report(input: StatusResignableReasonsInput):
+    with Session(engine) as session:
+        first_day = datetime.strptime(input.first_day, "%d-%m-%Y")
+
+        last_day = datetime.strptime(input.last_day, "%d-%m-%Y")
+
+        resignable_reasons_ids = json.loads(input.resignable_reasons_ids)
+
+        if 0 in resignable_reasons_ids:
+            resignable_reasons_ids = [
+                reason for reason in session.exec(select(ResignableReasons.id)).all()
+            ]
+
+        stmt = (
+            select(
+                Workers.id.label("worker_id"),
+                Workers.name.label("worker_name"),
+                Workers.resignation_date,
+                ResignableReasons.id.label("resignable_reason_id"),
+                ResignableReasons.name.label("resignable_reason_name"),
+            )
+            .join(
+                ResignableReasons,
+                Workers.resignation_reason_id == ResignableReasons.id,
+            )
+            .where(
+                and_(
+                    Workers.resignation_reason_id.in_(resignable_reasons_ids),
+                    Workers.resignation_date != None,
+                )
+            )
+        )
+
+        workers = session.exec(stmt).all()
+
+        results = [
+            {
+                "worker_id": worker.worker_id,
+                "worker_name": worker.worker_name,
+                "resignation_date": worker.resignation_date,
+                "resignable_reason_id": worker.resignable_reason_id,
+                "resignable_reason_name": worker.resignable_reason_name,
+            }
+            for worker in workers
+            if first_day
+            <= datetime.strptime(worker.resignation_date, "%d-%m-%Y")
+            <= last_day
+        ]
+
+    return results
--- main.py
+++ main.py
@@ -53,6 +53,10 @@ from controllers.jobs import (
     handle_post_job,
 )
 from controllers.months import handle_get_months
+from controllers.resignable_reasons import (
+    handle_get_resignable_reasons,
+    handle_resignable_reasons_report,
+)
 from controllers.roles import handle_get_roles
 from controllers.root import handle_activate_render_server, handle_get_docs_info
 from controllers.scale import (
@@ -126,6 +130,7 @@ from models.subsidiarie import Subsidiarie
 from models.turn import Turn
 from models.user import User
 from models.workers import Workers
+from pyhints.resignable_reasons import StatusResignableReasonsInput
 from pyhints.scales import (
     PostScaleInput,
     PostSomeWorkersScaleInput,
@@ -632,67 +637,13 @@ async def delete_department(id: int):
 
 
 @app.get("/resignable-reasons")
-def get_get_resignable_reasons():
-    with Session(engine) as session:
-        resignable_reasons = session.exec(select(ResignableReasons)).all()
-
-        return resignable_reasons
+async def get_resignable_reasons():
+    return await handle_database_operation(handle_get_resignable_reasons)
 
 
-class StatusResignableReasonsInput(BaseModel):
-    first_day: str
-    last_day: str
-    resignable_reasons_ids: str
+# resignable reasons reports
 
 
 @app.post("/resignable-reasons/report")
-def status_resignable_reasons(input: StatusResignableReasonsInput):
-    with Session(engine) as session:
-        first_day = datetime.strptime(input.first_day, "%d-%m-%Y")
-
-        last_day = datetime.strptime(input.last_day, "%d-%m-%Y")
-
-        resignable_reasons_ids = json.loads(input.resignable_reasons_ids)
-
-        if 0 in resignable_reasons_ids:
-            resignable_reasons_ids = [
-                reason for reason in session.exec(select(ResignableReasons.id)).all()
-            ]
-
-        stmt = (
-            select(
-                Workers.id.label("worker_id"),
-                Workers.name.label("worker_name"),
-                Workers.resignation_date,
-                ResignableReasons.id.label("resignable_reason_id"),
-                ResignableReasons.name.label("resignable_reason_name"),
-            )
-            .join(
-                ResignableReasons,
-                Workers.resignation_reason_id == ResignableReasons.id,
-            )
-            .where(
-                and_(
-                    Workers.resignation_reason_id.in_(resignable_reasons_ids),
-                    Workers.resignation_date != None,
-                )
-            )
-        )
-
-        workers = session.exec(stmt).all()
-
-        results = [
-            {
-                "worker_id": worker.worker_id,
-                "worker_name": worker.worker_name,
-                "resignation_date": worker.resignation_date,
-                "resignable_reason_id": worker.resignable_reason_id,
-                "resignable_reason_name": worker.resignable_reason_name,
-            }
-            for worker in workers
-            if first_day
-            <= datetime.strptime(worker.resignation_date, "%d-%m-%Y")
-            <= last_day
-        ]
-
-    return results
+async def get_resignable_reasons_report(input: StatusResignableReasonsInput):
+    return await handle_database_operation(handle_resignable_reasons_report, input)

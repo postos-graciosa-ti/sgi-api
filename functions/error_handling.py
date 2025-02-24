b1f6cfe2735eae8f5713ad62dcd8ff2a1b9c4c05
postos-graciosa-ti <postosgraciosati@gmail.com>
2025-02-24 07:49:56 -0300

    rrr

--- functions/error_handling.py
+++ functions/error_handling.py
@@ -9,7 +9,7 @@ logging.basicConfig(level=logging.ERROR)
 
 logger = logging.getLogger(__name__)
 
-# Decorador para capturar erros de banco de dados e outros erros inesperados
+
 def error_handler(func: Callable):
     @wraps(func)
     def wrapper(*args, **kwargs):
@@ -17,25 +17,26 @@ def error_handler(func: Callable):
             return func(*args, **kwargs)
 
         except NoResultFound:
-            # Erro: Nenhum resultado encontrado
             raise HTTPException(
                 status_code=404, detail="Resource not found with the given parameters"
             )
 
         except MultipleResultsFound:
-            # Erro: Múltiplos resultados encontrados quando se esperava um único
-            raise HTTPException(status_code=500, detail="Unexpected multiple results found")
+            raise HTTPException(
+                status_code=500, detail="Unexpected multiple results found"
+            )
 
         except SQLAlchemyError as e:
-            # Erro de banco de dados
             logger.error(f"Database error: {str(e)}")
+
             raise HTTPException(
-                status_code=500, detail="An error occurred while processing your request"
+                status_code=500,
+                detail="An error occurred while processing your request",
             )
 
         except Exception as e:
-            # Erro genérico para outros tipos de erro inesperados
             logger.error(f"Unexpected error: {str(e)}")
+
             raise HTTPException(
                 status_code=500,
                 detail="An unexpected error occurred. Please try again later",

c1c604a7ec955788169a55dbb6a385ecc0721ba6
postos-graciosa-ti <postosgraciosati@gmail.com>
2025-03-07 08:13:24 -0300

    fix

--- models/subsidiarie.py
+++ models/subsidiarie.py
@@ -2,6 +2,7 @@ from sqlmodel import Field, Session, SQLModel, create_engine, select
 
 class Subsidiarie(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
+    cnpj: str = Field(default=None, nullable=True)
     name: str = Field(index=True)
     adress: str = Field(index=True)
     phone: str = Field(index=True)
--- seeds/seed_all.py
+++ seeds/seed_all.py
@@ -38,6 +38,7 @@ def seed_subsidiaries():
             subsidiaries = [
                 Subsidiarie(
                     name="Posto Graciosa - Matriz",
+                    cnpj="76.608.660/0001-11",
                     adress="R. Florianópolis, 510 – Itaum, Joinville – SC, 89207-000",
                     phone="(47) 3436-0030",
                     email="matriz@postosgraciosa.com.br",
@@ -46,6 +47,7 @@ def seed_subsidiaries():
                 ),
                 Subsidiarie(
                     name="Auto Posto Fátima",
+                    cnpj="79.270.211/0001-02",
                     adress="R. Fátima, 1730 – Fátima, Joinville – SC, 89229-102",
                     phone="(47) 3466-0248",
                     email="fatima@postosgraciosa.com.br",
@@ -53,6 +55,7 @@ def seed_subsidiaries():
                 ),
                 Subsidiarie(
                     name="Posto Bemer",
+                    cnpj="81.512.683/0001-68",
                     adress="R. Boehmerwald, 675 – Boehmerwald, Joinville – SC, 89232-485",
                     phone="(47) 3465-0328",
                     email="bemer@postosgraciosa.com.br",
@@ -60,6 +63,7 @@ def seed_subsidiaries():
                 ),
                 Subsidiarie(
                     name="Posto Jariva",
+                    cnpj="04.123.127/0001-59",
                     adress="R. Monsenhor Gercino, 5085 – Jarivatuba, Joinville – SC, 89230-290",
                     phone="(47) 3466-4665",
                     email="jariva@postosgraciosa.com.br",
@@ -68,6 +72,7 @@ def seed_subsidiaries():
                 ),
                 Subsidiarie(
                     name="Posto Graciosa V",
+                    cnpj="84.708.437/0006-89",
                     adress="R. Santa Catarina, 1870 – Floresta, Joinville – SC, 89212-000",
                     phone="(47) 3436-1763",
                     email="graciosav@postosgraciosa.com.br",
@@ -76,6 +81,7 @@ def seed_subsidiaries():
                 ),
                 Subsidiarie(
                     name="Auto Posto Piraí",
+                    cnpj="11.168.652/0001-56",
                     adress="R. Quinze de Novembro, 5031 – Vila Nova, Joinville – SC, 89237-000",
                     phone="(47) 3422-9676",
                     email="pirai@postosgraciosa.com.br",

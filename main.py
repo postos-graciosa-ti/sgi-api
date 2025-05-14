b3f46a4cd8547588d96f507f03c708493559ce90
postos-graciosa-ti <postosgraciosati@gmail.com>
2025-05-14 16:54:00 -0300

    feature/add-new-selective-process

--- main.py
+++ main.py
@@ -3204,13 +3204,16 @@ def get_subsidiarie_metrics(id: int):
             .where(Workers.function_id == frentistas_function.id)
         ).all()
 
+        caixas_ideal = caixas_function.ideal_quantity or 9
+
+        frentistas_ideal = frentistas_function.ideal_quantity or 18
+
         return {
             "caixas_quantity": len(caixas_at_subsidiarie),
-            "caixas_ideal_quantity": caixas_function.ideal_quantity,
-            "has_caixas_ideal_quantity": len(caixas_at_subsidiarie)
-            >= caixas_function.ideal_quantity,
+            "caixas_ideal_quantity": caixas_ideal,
+            "has_caixas_ideal_quantity": len(caixas_at_subsidiarie) >= caixas_ideal,
             "frentistas_quantity": len(frentistas_at_subsidiarie),
-            "frentistas_ideal_quantity": frentistas_function.ideal_quantity,
+            "frentistas_ideal_quantity": frentistas_ideal,
             "has_frentistas_ideal_quantity": len(frentistas_at_subsidiarie)
-            >= frentistas_function.ideal_quantity,
+            >= frentistas_ideal,
         }
